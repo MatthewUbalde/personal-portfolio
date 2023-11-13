9b31f00c90813d88f7b3a4ba5960c005f9c96c36
FishHeadphones <matthew.ubalde@gmail.com>
2023-11-13 18:15:33 -0500

    Adjusted the structure and styles of the Gallery
    I made it so that the children can have its own container.
    
    Additionally, I created separate data for the frontend and backend.

--- src/pages/Web/webPage.tsx
+++ src/pages/Web/webPage.tsx
@@ -4,43 +4,61 @@ import TextArea from "../../components/TextArea/textarea";
 import SpotlightSection from "../../components/SpotlightSection/spotlightSection";
 import PreviewSection from "../../sections/PreviewSection/previewSection";
 
-import Data from "../../data/devPageThumbnails.json"
-import MintImg from "/assets/mint-big.png"
+import Data from "../../data/devPageThumbnails.json";
+import WebThumbnails from "../../data/thumbnails/webDevExperiences.json";
+import MintImg from "/assets/mint-big.png";
 import "./webPage.css";
 
 function WebPage() {
-  const thumbnailData: Array<ThumbnailData> = Data.thumbnails
+  const thumbnailData: Array<ThumbnailData> = Data.thumbnails;
+  const frontendThumbnails: Array<ThumbnailData> = WebThumbnails.frontendThumbnails;
 
   return (
     <div id="dev-page">
-      <Navbar sections={["Intro", "Examples", "Contacts"]} sectionType='dev' sectionSwitchTo='art'/>
-      
-      <SpotlightSection id='meet' backgroundImgPath={MintImg} backgroundImgAlt="Picture of Mint">
-        <h1>Matthew Ubalde</h1>
-        <p>A Programmer experienced in software, web, and game development.</p>   
+      <Navbar
+        sections={["Intro", "Experiences", "Examples", "Contacts"]}
+        sectionType="dev"
+        sectionSwitchTo="art"
+      />
+
+      <SpotlightSection
+        id="meet"
+        backgroundImgPath={MintImg}
+        backgroundImgAlt="Picture of Mint"
+      >
+        <h1>Web Development</h1>
+        <p>
+          I have experience in using <b>Vue</b> and <b>React</b> frameworks to
+          create websites like this
+        </p>
       </SpotlightSection>
-            
+      
+      <GallerySection id="experiences" thumbnails={frontendThumbnails}>
+        <div className="gallery-text-container">
+          <h2>Frontend Experience</h2>
+          <p>Here are my </p>
+        </div>
+      </GallerySection>
+
       <section id="intro">
-        <TextArea align='center'>
-            <p>Câline de bine de ciboire de caltor de cibole de christie d'estique de maudite marde de crucifix.</p>
-            <p>Crisse de marde de cul de christie d'esprit de saintes fesses de crucifix de viande à chien de calvince de boswell de gériboire. </p>
-        </TextArea>
-        <span>Lorem Ispum</span>
         <TextArea align="center">
-        <p>Câline de bine de ciboire de caltor de cibole de christie d'estique de maudite marde de crucifix.</p>
+          <h1>This website is created in React</h1>
+          <p>
+            This is my first website that I've created in React, and here is how I've learned so far in creating this website.
+          </p>
         </TextArea>
       </section>
 
-      <GallerySection id='examples' thumbnails={thumbnailData}>
+      <GallerySection id="examples" thumbnails={thumbnailData}>
         <h2>The many projects that I've made.</h2>
       </GallerySection>
 
       <section id="contacts">
-        <TextArea align='center'>
+        <TextArea align="center">
           <p>My contacts can be found here:</p>
         </TextArea>
       </section>
-      <PreviewSection/>
+      <PreviewSection />
     </div>
   );
 }
