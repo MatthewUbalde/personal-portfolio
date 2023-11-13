04b8a90cf06475bce2ebfc98423a48a538d687c9
FishHeadphones <matthew.ubalde@gmail.com>
2023-11-13 18:08:54 -0500

    Update types.tsx

--- src/lib/types.tsx
+++ src/lib/types.tsx
@@ -1,8 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 type ThumbnailData =
 { 
-  title?: string,
-  imgPath: string,
-  altImg: string,
-  link?: string
+  title?: string;
+  imgPath: string;
+  altImg: string;
+  link?: string;
 }
