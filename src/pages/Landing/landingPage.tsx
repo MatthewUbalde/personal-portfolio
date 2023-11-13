5f90ee555f9d56e968576bba7c7ff4b89ab271c1
FishHeadphones <matthew.ubalde@gmail.com>
2023-11-13 14:20:10 -0500

    Created a folder 'section' for Section-based components.
    Originally, I removed it because the section and components were similar by function back then (before I created a this new repo). Now however, sections, for me at least, are components that has the same information throughout the website. The PreviewSection is one of them.
    
    PreviewSection is a component that hold these PagePreviews which will be used throughout the website.
    
    Planning to rename the SpotlightSection for a better fitting name,

--- src/pages/Landing/landingPage.tsx
+++ src/pages/Landing/landingPage.tsx
@@ -1,9 +1,8 @@
 import Banner from "../../components/Banner/banner";
 import SpotlightSection from "../../components/SpotlightSection/spotlightSection";
-import PagePreview from "../../components/PagePreview/pagePreview";
-
 import MintImg from '/assets/mint-big.png'
 import "./landingPage.css";
+import PreviewSection from "../../sections/PreviewSection/previewSection";
 
 function App() {
   return (
@@ -14,27 +13,7 @@ function App() {
         <p>I also been exploring in practicing drawing illustrations, 3D Modelling as a hobby.</p>
         <p>Feel free to check both of them!</p>
       </SpotlightSection>
-      <div id="switch-buttons-container">
-        <PagePreview pageHref="dev" backgroundPath={MintImg}>
-          <h2>Programming Skills</h2>
-          <ul>
-            <li>C/C++, C#, Java</li>
-            <li>OpenGL</li>
-            <li>Vue React</li>
-            <li>MySQL</li>
-          </ul>
-        </PagePreview>
-        
-        <PagePreview pageHref="art" backgroundPath={MintImg}>
-          <h2>Artistic Skills</h2>
-          <ul>
-            <li>Used Krita</li>
-            <li>Basic understanding in 3D Modelling</li>
-            <li>Used Blender</li>
-            <li>Basic understanding in Illustrations</li>
-          </ul>
-        </PagePreview>
-      </div>
+      <PreviewSection/>
       <Banner status="construction" />
     </div>
   );
---
+++ src/sections/PreviewSection/previewSection.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import PagePreview from '../../components/PagePreview/pagePreview'
+import MintImg from '/assets/mint-big.png'
+import './previewSection.css'
+
+function PreviewSection() {
+  return (
+    <section className="preview-section">
+        <PagePreview pageHref="dev" backgroundPath={MintImg}>
+          <h2>Web Development</h2>
+          <ul>
+            <li>C/C++, C#, Java</li>
+            <li>OpenGL</li>
+            <li>Vue React</li>
+            <li>MySQL</li>
+          </ul>
+        </PagePreview>
+        <PagePreview pageHref="art" backgroundPath={MintImg}>
+          <h2>Artistic Skills</h2>
+          <ul>
+            <li>Used Krita</li>
+            <li>Basic understanding in 3D Modelling</li>
+            <li>Used Blender</li>
+            <li>Basic understanding in Illustrations</li>
+          </ul>
+        </PagePreview>
+    </section>
+  )
+}
+
+export default PreviewSection
