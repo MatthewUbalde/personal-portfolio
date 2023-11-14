2568bb58e6d2ad71895aa08b6cf536efb26b309f
FishHeadphones <matthew.ubalde@gmail.com>
2023-11-14 10:51:45 -0500

    Updated PreviewSection related elements.
    Added a title for the PreviewSection and adjusted the PagePreviews

--- src/sections/PreviewSection/previewSection.tsx
+++ src/sections/PreviewSection/previewSection.tsx
@@ -1,15 +1,21 @@
-import PagePreview from '../../components/PagePreview/pagePreview'
-import MintImg from '/assets/mint-big.png'
-import './previewSection.css'
+import PagePreview from "../../components/PagePreview/pagePreview";
+import MintImg from "/assets/mint-big.png";
+import "./previewSection.css";
 
 interface Props {
-  id?: string
+  id?: string;
 }
 
-function PreviewSection({id}: Props) {
+function PreviewSection({ id }: Props) {
   return (
     <section id={id} className="preview-section">
-        <PagePreview routePath="web" bgImgPath={MintImg} bgImgAlt='Web Development Background'>
+      <h1>Other Works</h1>
+      <div className="page-previews-container">
+        <PagePreview
+          routePath="web"
+          bgImgPath={MintImg}
+          bgImgAlt="Web Development Background"
+        >
           <h2>Web Development</h2>
           <h3>Frontend</h3>
           <ul>
@@ -22,14 +28,22 @@ function PreviewSection({id}: Props) {
             <li>React</li>
           </ul>
         </PagePreview>
-        <PagePreview routePath="" bgImgPath={MintImg} bgImgAlt='Game Development Background'>
+        <PagePreview
+          routePath=""
+          bgImgPath={MintImg}
+          bgImgAlt="Game Development Background"
+        >
           <h2>Game Development</h2>
           <ul>
             <li>Godot</li>
             <li>Unity</li>
           </ul>
         </PagePreview>
-        <PagePreview routePath="" bgImgPath={MintImg} bgImgAlt='Software Development Background'>
+        <PagePreview
+          routePath=""
+          bgImgPath={MintImg}
+          bgImgAlt="Software Development Background"
+        >
           <h2>Software Development</h2>
           <ul>
             <li>C/C++, C#, Java</li>
@@ -37,15 +51,20 @@ function PreviewSection({id}: Props) {
             <li>MySQL</li>
           </ul>
         </PagePreview>
-        <PagePreview routePath="" bgImgPath={MintImg} bgImgAlt='Artistic Skills Background'>
+        <PagePreview
+          routePath=""
+          bgImgPath={MintImg}
+          bgImgAlt="Artistic Skills Background"
+        >
           <h2>Artistic Skills</h2>
           <ul>
             <li>Used Krita at a Intermiedate Level</li>
             <li>Used Blender at a Basic Level</li>
           </ul>
         </PagePreview>
+      </div>
     </section>
-  )
+  );
 }
 
-export default PreviewSection
+export default PreviewSection;
