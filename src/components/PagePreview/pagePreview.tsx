352b2f2432b4e352fb3f79ad02606de8f7365282
FishHeadphones <matthew.ubalde@gmail.com>
2023-11-13 20:53:10 -0500

    Update pagePreview.tsx
    Reviewed how Hooks are used properly in React.

--- src/components/PagePreview/pagePreview.tsx
+++ src/components/PagePreview/pagePreview.tsx
@@ -11,23 +11,21 @@ interface Props {
 }
 
 function PagePreview({ children, routePath, bgImgPath, bgImgAlt }: Props) {
-  if (!routePath)
-    return <></>;
-    
+  
   const navigate = useNavigate();
-  const routeChange = () => {
+  function routeChange() {
     const path: string = "/personal-portfolio/" + routePath;
     navigate(path);
   }
-
+  
+  if (!routePath) return <></>;
+  
   return (
     <div onClick={routeChange} className="page-preview">
       <div className="page-preview-placeholder">
-        <div className="page-preview-container">
-          {children}
-        </div>
+        <div className="page-preview-container">{children}</div>
       </div>
-      <img className="page-preview-bg" src={bgImgPath} alt={bgImgAlt}/>
+      <img className="page-preview-bg" src={bgImgPath} alt={bgImgAlt} />
       {/* <div
         className="page-preview-bg"
         style={{ backgroundImage: `url(${backgroundPath})` }}
