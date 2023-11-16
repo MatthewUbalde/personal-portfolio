e275d72380e5da06b3289b648510a07a2d690500
FishHeadphones <matthew.ubalde@gmail.com>
2023-11-16 15:02:32 -0500

    Comment out the other pages
    They're not needed at the moment and probably contribute to the long loading times. Will implement async lazy loading when there's more of them.

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,9 @@ import ReactDOM from 'react-dom/client'
 import Banner from './components/Banner/banner.tsx'
 import LandingPage from './pages/Landing/landingPage.tsx'
 import WebPage from './pages/Web/webPage.tsx'
-import ArtPage from './pages/Art/artPage.tsx'
-import GamePage from './pages/Game/gamePage.tsx'
-import SoftwarePage from './pages/Software/softwarePage.tsx'
+// import ArtPage from './pages/Art/artPage.tsx'
+// import GamePage from './pages/Game/gamePage.tsx'
+// import SoftwarePage from './pages/Software/softwarePage.tsx'
 import './index.css'
 
 const router = createBrowserRouter([
@@ -17,19 +17,20 @@ const router = createBrowserRouter([
   {
     path: "/personal-portfolio/web",
     element: <WebPage/>,
-  },
-  {
-    path: "/personal-portfolio/game",
-    element: <GamePage/>,
-  },
-  {
-    path: "/personal-portfolio/software",
-    element: <SoftwarePage/>,
-  },
-  {
-    path: "/personal-portfolio/art",
-    element: <ArtPage/>,
-  },
+  }
+  // },
+  // {
+  //   path: "/personal-portfolio/game",
+  //   element: <GamePage/>,
+  // },
+  // {
+  //   path: "/personal-portfolio/software",
+  //   element: <SoftwarePage/>,
+  // },
+  // {
+  //   path: "/personal-portfolio/art",
+  //   element: <ArtPage/>,
+  // },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
