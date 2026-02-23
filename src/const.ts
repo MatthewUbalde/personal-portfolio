5e3e9a07f095ec0034abd5cc9b2588906d3e46ec
Matthew Ubalde <113562981+MatthewUbalde@users.noreply.github.com>
2026-02-23 13:50:28 -0500

    Restart personal-portfolio from beginning

---
+++ src/const.ts
@@ -0,0 +1,64 @@
+import type { NavLinkType } from "./types";
+
+/**
+ * Holds the Navigation tree that the user can follow
+ */
+const SITE_ROOT: string = "/personal-portfolio-astro";
+export const SITE_NAVIGATION: Array<NavLinkType> = [
+  {
+    href: `${SITE_ROOT}/`,
+    label: "Home",
+  },
+  {
+    href: `${SITE_ROOT}/career`,
+    label: "Career",
+    children: [
+      {
+        href: "/web",
+        label: "Web",
+      },
+      {
+        href: "/software",
+        label: "Software",
+      },
+      {
+        href: "/mobile",
+        label: "Mobile",
+      },
+      {
+        href: "/game",
+        label: "Game",
+      },
+      {
+        href: "/art",
+        label: "Art",
+      },
+    ],
+  },
+  {
+    href: `${SITE_ROOT}/projects`,
+    label: "Projects",
+    children: [
+      {
+        href: "/apps",
+        label: "Applications",
+      },
+      {
+        href: "/games",
+        label: "Games",
+      },
+      {
+        href: "/websites",
+        label: "Websites",
+      },
+    ],
+  },
+];
+
+/**
+ * All of the social links that directly points to me
+ */
+export const SOCIAL_MEDIA_LINKS = {
+  github: "https://github.com/MatthewUbalde",
+  linkedin: "https://www.linkedin.com/in/matthew-ubalde-171918252/",
+};
