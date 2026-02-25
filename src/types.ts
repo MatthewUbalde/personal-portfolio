a3041de5372ad9f0b76f25d9f4b2be41b56e075d
Matthew Ubalde <113562981+MatthewUbalde@users.noreply.github.com>
2026-02-24 21:59:52 -0500

    Bring back the types.ts from prev repository

---
+++ src/types.ts
@@ -0,0 +1,50 @@
+import type { ImageMetadata } from "astro";
+
+export type ImageLinkType = {
+  src: Promise<{ default: ImageMetadata }> | ImageMetadata;
+  alt?: string;
+};
+
+export type ProjectShowcaseType = {
+  title: string;
+  description: string;
+  coverImage: Promise<{ default: ImageMetadata }> | ImageMetadata;
+  coverImageAlt: string;
+  href: string;
+  published: Date;
+  itchioLink?: string;
+  steamLink?: string;
+  githubLink?: string;
+};
+
+export type ExperienceBadgeType = {
+  title: string;
+  src: string;
+  experience: "Learning" | "Beginner" | "Adequate" | "Intermediate" | "Expert";
+};
+
+export type NavLinkType = {
+  href: string;
+  label: string;
+  children?: NavLinkType[];
+};
+
+export type MalleableComponentType = {
+  noPadding?: boolean | undefined;
+  noMargin?: boolean | undefined;
+  noRounded?: boolean | undefined;
+};
+
+export type ContentComponentType = {
+  textContent?: boolean | undefined;
+  articleContent?: boolean | undefined;
+};
+
+export type FlexComponentType = {
+  col?: boolean;
+  row?: boolean;
+};
+
+export type MediaQueryComponentType = {
+  noMedia?: boolean | undefined;
+};
